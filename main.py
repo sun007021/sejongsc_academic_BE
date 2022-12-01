2d7f087dc3801c6a82de05f8d26867682ecb58fd
sun007021 <sun007021@gmail.com>
2022-12-01 14:13:10 +0900

    style : 줄 정렬

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI, Query #import fastapi framework
+from fastapi import FastAPI, Query  # import fastapi framework
 from typing import Union
 
 from domain.otp import otp_router
 from domain.user import user_router
 
 app = FastAPI()
-
 
 app.include_router(user_router.router)
 app.include_router(otp_router.router)
