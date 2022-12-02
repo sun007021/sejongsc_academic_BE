fbe063e89be8c21efbb8c706490aa04b398cc192
sun007021 <sun007021@gmail.com>
2022-12-03 01:45:40 +0900

    style: 줄바꿈 및 미완성 기능 임시 값 반환

--- domain/visits/visits_router.py
+++ domain/visits/visits_router.py
@@ -38,7 +38,7 @@ def update_end_time(visit_id: int = Path(title="visit id"), db: Session = Depend
     visits_crud.update_end_time(db, visit_id)
 
 
-@router.get("/recent/{booth_id}", status_code = status.HTTP_200_OK)
+@router.get("/recent/{booth_id}", status_code=status.HTTP_200_OK)
 def get_recent_visit_user_num(booth_id: int = Path(title="booth id"), db: Session = Depends(get_db)):
-    return visits_crud.get_recent_booth_visit_user_number(db,booth_id)
-
+    # return visits_crud.get_recent_booth_visit_user_number(db, booth_id)
+    return {"recent_visit_user_num": 7}
