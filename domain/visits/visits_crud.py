508e0962383eddd9fca7a24800c7f2b8c9360c09
sun007021 <sun007021@gmail.com>
2022-12-05 10:12:43 +0900

    feat: 최근 30분내 방문객 수 계산

--- domain/visits/visits_crud.py
+++ domain/visits/visits_crud.py
@@ -14,7 +14,7 @@ def get_visit(db: Session, visit_id: int):
 
 
 def get_recent_booth_visit_user_number(db: Session, booth_id: int):
-    recent_visit_user_num = db.query(Visit).filter(and_((Visit.booth_id == booth_id),((datetime.datetime.now() - Visit.start_time).seconds <= 1800))).count()
+    recent_visit_user_num = db.query(Visit).filter(and_((Visit.booth_id == booth_id),((datetime.datetime.now() - Visit.start_time).total_seconds() <= 1800))).count()
     return recent_visit_user_num
 
 def get_user_visits_booth(db: Session, username: str):  # 해당 유저가 방문한 기록 중 endtime 존재하는 것들 반환
